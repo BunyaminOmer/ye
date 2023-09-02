09338cc0a57685a97690eee5dcdd05ab4a9f0be5
BunyaminOmer <bunyaminomerkarakus@protonmail.com>
2023-09-02 21:28:13 +0300

    first commit

---
+++ dene.py
@@ -0,0 +1,559 @@
+class a:
+    def __init__(self, isim):
+        # Yapıcı method (constructor): isim adında bir özellik oluşturuyor.
+        self.isim = isim
+
+class b:
+    soyisim = "Ömer" # (class attribute) tanımladı.
+
+class c(a, b):
+    pass
+
+nesne1 = a("Ömer")
+print(nesne1.isim)
+print(b.soyisim)
+
+# 2
+
+class d:
+    def __init__(self, age): # Yapıcı method (constructor): isim adında bir özellik oluşturuyor.
+        self.age = age
+
+class e:
+    renk = "Blue" # (class attribute) tanımladı.
+
+class f(d, e):
+    pass
+
+nesne2 = d(26)
+print(nesne2.age)
+print(e.renk)
+# 3
+
+class g:
+    def __init__(self, film): # Yapıcı method (constructor) oluşturdu.
+        self.film = film
+
+class ğ:
+    sarki = "Enercii" # (class attribute) tanımladı.
+
+class h(g, ğ):
+    pass
+
+nesne3 = g("Görevimiz Tatil")
+print(nesne3.film)
+print(ğ.sarki)
+
+# 4
+
+class ı:
+    def __init__(self, spor): # Yapıcı method (constructor) oluşturdu.
+        self.spor = spor
+
+class i:
+    colour2 = "Black" # (class attribute) tanımladı.
+
+class j(ı, i):
+    pass
+
+nesne4 = ı("jdkw")
+print(nesne4.spor)
+print(i.colour2)
+
+# 5
+
+class k:
+    def __init__(self, cins): # Yapıcı method (constructor) oluşturdu.
+        self.cins = cins
+
+class l:
+    colour3 = "Yellow" # (class attribute) tanımladı.
+
+class m(k, l):
+    pass
+
+nesne5 = k("E")
+print(nesne5.cins)
+print(l.colour3)
+
+# 6
+
+class n:
+    def __init__(self, sehir): # Yapıcı method (constructor) oluşturdu.
+        self.sehir = sehir
+
+class o:
+    dene = "Bul" # (class attribute) tanımladı.
+
+class ö(n, o):
+    pass
+
+nesne6 = n("Bursa")
+print(nesne6.sehir)
+print(o.dene)
+
+# 7
+
+class p:
+    def __init__(self, elbise): # Yapıcı method (constructor) oluşturdu.
+        self.elbise = elbise
+
+class r:
+    find = True # (class attribute) tanımladı.
+
+class s(p, r):
+    pass
+
+nesne7 = p("Kot")
+print(nesne7.elbise)
+print(r.find)
+
+# 8
+
+class ş:
+    def __init__(self, place): # Yapıcı method (constructor) oluşturdu.
+        self.place = place
+
+class t:
+    deneme = "JWI" # (class attribute) tanımladı.
+
+class u:
+    pass
+
+nesne8 = ş("Bdsa")
+print(nesne8.place)
+print(t.deneme)
+
+# 9
+
+class ü:
+    def __init__(self, grenk): # Yapıcı method (constructor) oluşturdu.
+        self.grenk = grenk
+
+
+class v:
+    fsaj = 672.5 # (class attribute) tanımladı.
+
+class y(ü, v):
+    pass
+
+nesne9 = ü("Blue")
+print(nesne9.grenk)
+print(v.fsaj)
+
+# 10
+
+class aa:
+    def __init__(self, meyve):  # Yapıcı method (constructor) oluşturdu.
+        self.meyve = meyve
+
+class bb:
+    ds = 2j # (class attribute) tanımladı.
+
+class cc(aa, bb):
+    pass
+
+nesne10 = aa("Çilek")
+print(nesne10.meyve)
+print(bb.ds)
+
+# 11
+
+class çç:
+    def __init__(self, araba):  # Yapıcı method (constructor) oluşturdu.
+        self.araba = araba
+
+class dd:
+    okadn = 78 # (class attribute) tanımladı.
+
+class ee(çç, dd):
+    pass
+
+nesne11 = çç("Bmw")
+print(nesne11.araba)
+print(dd.okadn)
+
+# 12
+
+class ff:
+    def __init__(self, dizi): # Yapıcı method (constructor) oluşturdu.
+        self.dizi = dizi
+
+class gg:
+    gyuak = "dmna" # (class attribute) tanımladı.
+
+class ğğ(ff, gg):
+    pass
+
+nesne12 = ff("Kendi Düşen Ağlamaz")
+print(nesne12.dizi)
+print(gg.gyuak)
+
+# 13
+
+class hh:
+    def __init__(self, kitap): # Yapıcı method (constructor) oluşturdu.
+        self.kitap = kitap
+
+class ıı:
+    djs = 892j # (class attribute) tanımladı.
+
+class ii(hh, ıı):
+    pass
+
+nesne13 = hh("SNAj")
+print(nesne13.kitap)
+print(ıı.djs)
+
+# 14
+
+class jj:
+    def __init__(self, saat): # Yapıcı method (constructor) oluşturdu.
+        self.saat = saat
+
+class kk:
+    jdw = 617j # (class attribute) tanımladı.
+
+class ll(jj, kk):
+    pass
+
+nesne14 = jj("17.53")
+print(nesne14.saat)
+print(kk.jdw)
+
+# 15
+
+class mm:
+    def __init__(self, eat): # Yapıcı method (constructor) oluşturdu.
+        self.eat = eat
+
+class nn:
+    dnaj = 61 # (class attribute) tanımladı.
+
+class oo(mm,nn):
+    pass
+
+nesne15 = mm("Ye")
+print(nesne15.eat)
+print(nn.dnaj)
+
+# 16
+
+class öö:
+    def __init__(self, içecek): # Yapıcı method (constructor) oluşturdu.
+        self.içecek = içecek
+
+
+class pp:
+    sjai = {"ajs",53,2j} # (class attribute) tanımladı.
+
+class rr(öö, pp):
+    pass
+
+nesne16 = öö("nescafe")
+print(nesne16.içecek)
+print(pp.sjai)
+
+# 17
+
+class ss:
+    def __init__(self, boy): # Yapıcı method (constructor) oluşturdu.
+        self.boy = boy
+
+class şş:
+    dhw = 1712 # (class attribute) tanımladı.
+
+class tt(ss, şş):
+    pass
+
+nesne17 = ss(1.68)
+print(nesne17.boy)
+print(şş.dhw)
+
+# 18
+
+class uu:
+    def __init__(self,ayakno): # Yapıcı method (constructor) oluşturdu.
+        self.ayakno = ayakno
+
+class üü:
+    smaj = 781 # (class attribute) tanımladı.
+
+class vv(uu, üü):
+    pass
+
+nesne18 = uu(42)
+print(nesne18.ayakno)
+print(üü.smaj)
+
+# 19
+
+class yy:
+    def __init__(self, mekan): # Yapıcı method (constructor) oluşturdu.
+        self.mekan = mekan
+
+class zz:
+    samjı = 2237j # (class attribute) tanımladı.
+
+class adaa(yy, zz):
+    pass
+
+nesne19 = yy("Mekan")
+print(nesne19.mekan)
+print(zz.samjı)
+
+# 20
+
+class aaa:
+    def __init__(self, gamename): # Yapıcı method (constructor) oluşturdu.
+        self.gamename = gamename
+
+class bbb:
+    saka = 217j # (class attribute) tanımladı.
+
+class ccc:
+    pass
+
+nesne20 = aaa("BS")
+print(nesne20.gamename)
+print(bbb.saka)
+
+# 21
+
+class ççç:
+    def __init__(self, sigaraeh): # Yapıcı method (constructor) oluşturdu.
+        self.sigaraeh = sigaraeh
+
+
+class ddd:
+    nsj = 2789j # (class attribute) tanımladı.
+
+class eee(ççç, ddd):
+    pass
+
+nesne21 = ççç(False)
+print(nesne21.sigaraeh)
+print(ddd.nsj)
+
+# 22
+
+class fff:
+    def __init__(self, sporA): # Yapıcı method (constructor) oluşturdu.
+        self.sporA = sporA
+
+class ggg:
+    dmsnj = 178 # (class attribute) tanımladı.
+
+class ğğğ(fff, ggg):
+    pass
+
+nesne22 = fff("Ferrari")
+print(nesne22.sporA)
+print(ggg.dmsnj)
+
+# 23
+
+class hhh:
+    def __init__(self, tercih): # Yapıcı method (constructor) oluşturdu.
+        self.tercih = tercih
+
+class ııı:
+    dmaj = 231 # (class attribute) tanımladı.
+
+class iii(hhh, ııı):
+    pass
+
+nesne23 = hhh("Yok")
+print(nesne23.tercih)
+print(ııı.dmaj)
+
+# 24
+
+class jjj:
+    def __init__(self, hobi): # Yapıcı method (constructor) oluşturdu.
+        self.hobi = hobi
+
+class kkk:
+    dmş = 123 # (class attribute) tanımladı.
+
+class lll(jjj, kkk):
+    pass
+
+nesne24 = jjj("Yüzme")
+print(nesne24.hobi)
+print(kkk.dmş)
+
+# 25
+class mmm:
+    def __init__(self, içecek): # Yapıcı method (constructor) oluşturdu.
+        self.içecek = içecek
+
+class nnn:
+    fjs = "Kahve" # (class attribute) tanımladı.
+
+class ooo(mmm, nnn):
+    pass
+
+nesne25 = mmm("Çay")
+print(nesne25.içecek)
+print(nnn.fjs)
+
+# 26
+
+class ppp:
+    def __init__(self, renk): # Yapıcı method (constructor) oluşturdu.
+        self.renk = renk
+
+class qqq:
+    skds = "Sarı" # (class attribute) tanımladı.
+
+class rrr(ppp, qqq):
+    pass
+
+nesne26 = ppp("Kırmızı")
+print(nesne26.renk)
+print(qqq.skds)
+
+# 27
+
+class sss:
+    def __init__(self, yemek): # Yapıcı method (constructor) oluşturdu.
+        self.yemek = yemek
+
+class ttt:
+    djsa = "Pizza" # (class attribute) tanımladı.
+
+class uuu(sss, ttt):
+    pass
+
+nesne27 = sss("Makarna")
+print(nesne27.yemek)
+print(ttt.djsa)
+
+# 28
+
+class vvv:
+    def __init__(self, yer): # Yapıcı method (constructor) oluşturdu.
+        self.yer = yer
+
+class www:
+    fsd = "Ankara" # (class attribute) tanımladı.
+
+class xxx(vvv, www):
+    pass
+
+nesne28 = vvv("İstanbul")
+print(nesne28.yer)
+print(www.fsd)
+
+# 29
+
+class yyy:
+    def __init__(self, araç): # Yapıcı method (constructor) oluşturdu.
+        self.araç = araç
+
+class zzz:
+    gsj = "Bisiklet" # (class attribute) tanımladı.
+
+class aaaa(yyy, zzz):
+    pass
+
+nesne29 = yyy("Otomobil")
+print(nesne29.araç)
+print(zzz.gsj)
+
+# 30
+
+class bbbb:
+    def __init__(self, sebze): # Yapıcı method (constructor) oluşturdu.
+        self.sebze = sebze
+
+class cccc:
+    saj = "Elma" # (class attribute) tanımladı.
+
+class dddd(bbbb, cccc):
+    pass
+
+nesne30 = bbbb("Muz")
+print(nesne30.sebze)
+print(cccc.saj)
+
+# 31
+
+class hhhh:
+    def __init__(self, yiyecek): # Yapıcı method (constructor) oluşturdu.
+        self.yiyecek = yiyecek
+
+class iiii:
+    djks = "Pasta" # (class attribute) tanımladı.
+
+class jjjj(hhhh, iiii):
+    pass
+
+nesne32 = hhhh("Kek")
+print(nesne32.yiyecek)
+print(iiii.djks)
+
+# 32
+
+class nnnn:
+    def __init__(self, ülke): # Yapıcı method (constructor) oluşturdu.
+        self.ülke = ülke
+
+class oooo:
+    jdka = "Türkiye" # (class attribute) tanımladı.
+
+class pppp(nnnn, oooo):
+    pass
+
+nesne34 = nnnn("İtalya")
+print(nesne34.ülke)
+print(oooo.jdka)
+
+# 33
+
+class zzzz:
+    def __init__(self, kelime): # Yapıcı method (constructor) oluşturdu.
+        self.kelime = kelime
+
+class aaaaa:
+    jdsa = "Python" # (class attribute) tanımladı.
+
+class bbbbb(zzzz, aaaaa):
+    pass
+
+nesne38 = zzzz("Java")
+print(nesne38.kelime)
+print(aaaaa.jdsa)
+
+# 34
+
+class vvvv:
+    def __init__(self, hayvan):
+        self.hayvan = hayvan
+
+class yyyy:
+    njid = 781j
+
+class xxxx(vvvv, yyyy):
+    pass
+
+nesne31 = vvvv("Kedi")
+print(nesne31.hayvan)
+print(yyyy.njid)
+
+# 35
+
+class qqqq:
+    def __init__(self, ünlü):
+        self.ünlü = ünlü
+
+class wwww:
+    ja = 2j
+
+class hahaa(qqqq, wwww):
+    pass
+
+nesne33 = qqqq("Mauro İcardi")
+print(nesne33.ünlü)
+print(wwww.ja)
